f26e4c3f4a6f8e6985d7d8155786ec681304aa43
ChanHyuckYou <qqs4562@naver.com>
2024-05-23 21:23:25 +0900

    chanhyck : tnwjd

--- src/pages/user/MenuEdit.js
+++ src/pages/user/MenuEdit.js
@@ -9,10 +9,16 @@ export default function MenuEditPage() {
     const ownerid = localStorage.getItem('ownerid');
     const [menus, setMenus] = useState([]); // 메뉴 정보를 저장할 상태
 
+    const goToMenuEdit = (selectedMenu) => {
+        localStorage.setItem('ownerid', ownerid);
+        localStorage.setItem('productid', selectedMenu.productid);
+        console.log("선택된 productid :", selectedMenu.productid);
+        // navigate 함수를 통해 선택한 menu만을 상태로 전달합니다.
+        navigate('/Main/Menu/Add', { state: { selectedMenu } });
+    };
 
 
-
-    // 컴포넌트가 마운트될 때 메뉴 정보를 가져옴
+// 컴포넌트가 마운트될 때 메뉴 정보를 가져옴
     useEffect(() => {
         // 메뉴 정보를 가져오는 함수
         const fetchMenus = async () => {
@@ -40,6 +46,7 @@ export default function MenuEditPage() {
         navigate(-1);
     };
 
+
     return (
         <div className="menuEdit">
             <div className="headerContainer">
@@ -63,24 +70,24 @@ export default function MenuEditPage() {
             </div>
             <div className="container-14">
                 <div className="recommended-menu">
-                    <span className="recommended">
-                        추천 메뉴
-                    </span>
+                <span className="recommended">
+                    추천 메뉴
+                </span>
                 </div>
                 <div className="main-menu">
-                    <span className="main">
-                        주 메뉴
-                    </span>
+                <span className="main">
+                    주 메뉴
+                </span>
                 </div>
                 <div className="side-tab">
-                    <span className="side">
-                        사이드
-                    </span>
+                <span className="side">
+                    사이드
+                </span>
                 </div>
                 <button className="tab-add-bt" type="button">
-                    <span className="tab-add">
-                        + 탭 추가
-                    </span>
+                <span className="tab-add">
+                    + 탭 추가
+                </span>
                 </button>
             </div>
             <div className="container-7">
@@ -88,26 +95,26 @@ export default function MenuEditPage() {
                     메뉴 관리
                 </div>
                 <button className="menu-add-bt" onClick={goToMenuAdd} style={{cursor: 'pointer'}}>
-                    <span className="menu-add">
-                        메뉴 추가
-                    </span>
+                <span className="menu-add">
+                    메뉴 추가
+                </span>
                 </button>
             </div>
             <div className="menu-list">
                 {menus.map((menu) => (
                     <div className="menu-1" key={menu.productid}>
                         <div className="container-6">
-                                {/* 이미지 URL이 있다면 아래와 같이 사용할 수 있습니다. */}
-                                <img
-                                    className="menu-1-image"
-                                     src={menu.imageurl} alt={""} />
+                            {/* 이미지 URL이 있다면 아래와 같이 사용할 수 있습니다. */}
+                            <img
+                                className="menu-1-image"
+                                src={menu.imageurl} alt={""} />
                             <div className="container-4">
                                 <div className="menu-1-name">
                                     {menu.productname}
                                 </div>
                                 <span className="menu-1-info">
-                                    {menu.description}
-                                </span>
+                                {menu.description}
+                            </span>
                             </div>
                         </div>
                         <div className="container-18">
@@ -116,20 +123,20 @@ export default function MenuEditPage() {
                             </div>
                             <div className="container-3">
                                 <div className="container-10">
-                                    <button className="edit-bt-1" type="button" style={{cursor: 'pointer'}}>
-                                        <span className="edit-1">
-                                            수정
-                                        </span>
+                                    <button className="edit-bt-1" type="button" style={{cursor: 'pointer'}} onClick={() => goToMenuEdit(menu)}>
+                                    <span className="edit-1">
+                                        수정
+                                    </span>
                                     </button>
                                     <button className="del-bt" type="button" style={{cursor: 'pointer'}}>
-                                        <span className="del-1">
-                                            삭제
-                                        </span>
+                                    <span className="del-1">
+                                        삭제
+                                    </span>
                                     </button>
                                 </div>
                                 <span className="menu-1-price">
-                                    <PriceComponent price={menu.price} />
-                                </span>
+                                <PriceComponent price={menu.price} />
+                            </span>
                             </div>
                         </div>
                     </div>
@@ -139,8 +146,9 @@ export default function MenuEditPage() {
     );
 }
 function PriceComponent({ price }) {
-    // 숫자를 지역화된 문자열로 변환하여 3자리마다 쉼표를 추가
+// 숫자를 지역화된 문자열로 변환하여 3자리마다 쉼표를 추가
     const formattedPrice = parseInt(price, 10).toLocaleString();
+
 
     return <div>{formattedPrice}원</div>;
 }
